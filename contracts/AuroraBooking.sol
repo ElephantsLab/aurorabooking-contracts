b47a8717d9b326ecc7614bc64e869951630b8cd8
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-12-16 18:26:42 +0200

    Main contract logic implemented

--- contracts/AuroraBooking.sol
+++ contracts/AuroraBooking.sol
@@ -4,6 +4,25 @@ pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+import "./AuroraBookingNFT.sol";
+
 contract AuroraBooking is Ownable {
+
+  address nftContractAddress;
+  uint256 nftCounter;
+
+  event Booked(address indexed buyer, uint8 placeId, uint8 tableId, uint256 nftId);
+
+  constructor(address _nftContractAddress) {
+    nftContractAddress = _nftContractAddress;
+  }
+
+  function book(uint8 placeId, uint8 tableId) external {
+    AuroraBookingNFT(nftContractAddress).mint(msg.sender, nftCounter);
+
+    emit Booked(msg.sender, placeId, tableId, nftCounter);
+
+    nftCounter++;
+  }
 
 }
