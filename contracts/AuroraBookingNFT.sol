5431eaa06e68f3976d61e5ac8f510e6dbdac5122
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-12-16 14:46:00 +0200

    Initial contracts prototypes

---
+++ contracts/AuroraBookingNFT.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract AuroraBookingNFT is ERC721("Aurora Booking Collection", "ABC") {
+  
+}
