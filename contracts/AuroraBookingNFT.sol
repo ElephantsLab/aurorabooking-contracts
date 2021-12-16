497caca9aa39da555ff79aaabf88941146a63fbe
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-12-16 18:13:24 +0200

    NFT token logic implemented

--- contracts/AuroraBookingNFT.sol
+++ contracts/AuroraBookingNFT.sol
@@ -2,8 +2,45 @@
 
 pragma solidity >=0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract AuroraBookingNFT is ERC721("Aurora Booking Collection", "ABC") {
-  
+contract AuroraBookingNFT is ERC721("Aurora Booking Collection", "ABC"), Ownable {
+
+  mapping(address => uint256[]) public owns;
+
+  function _baseURI() internal pure override returns (string memory) {
+    return "http://aurorabooking.net/metadata/";
+  }
+
+  function _transfer(
+    address from,
+    address to,
+    uint256 tokenId
+  ) internal override {
+    super._transfer(from, to, tokenId);
+
+    uint8 idx = 0;
+    while (idx < owns[from].length) {
+      if (owns[from][idx] == tokenId) {
+        break;
+      }
+
+      idx++;
+    }
+
+    if (idx < owns[from].length) {
+      owns[from][idx] = owns[from][owns[from].length - 1];
+      owns[from].pop();
+    }
+
+    owns[to].push(tokenId);
+  }
+
+  function mint(address _address, uint256 _id) external onlyOwner {
+    _mint(_address, _id);
+  }
+
+  //TODO: burning
+
 }
