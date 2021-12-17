b995517540e0ff4058eca5847fcfaeb0a7033bea
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2021-12-17 21:23:43 +0200

    Sell/buy mechanism fixes

--- contracts/AuroraBooking.sol
+++ contracts/AuroraBooking.sol
@@ -22,7 +22,7 @@ contract AuroraBooking is Ownable {
   Lot[] public lots;
 
   event Booked(address indexed buyer, uint8 placeId, uint8 tableId, uint256 tokenId);
-  event LotCreated(address indexed seller, uint256 tokenId, uint256 price);
+  event LotCreated(address indexed seller, uint256 indexed tokenId, uint256 price, uint256 indexed lotId);
 
   constructor(address _nftContractAddress) {
     nftContractAddress = _nftContractAddress;
@@ -54,12 +54,15 @@ contract AuroraBooking is Ownable {
       isSoldOut: false
     }));
 
-    emit LotCreated(msg.sender, tokenId, price);
+    emit LotCreated(msg.sender, tokenId, price, lots.length - 1);
   }
 
   function buy(uint256 lotId) external payable {
+    require(!lots[lotId].isSoldOut, "This lot is already sold out");
     require(lots[lotId].seller == AuroraBookingNFT(nftContractAddress).ownerOf(lots[lotId].tokenId), "Seller isn't an owner of this NFT");
     require(msg.value == lots[lotId].price, "Invalid ETH amount");
+
+    lots[lotId].isSoldOut = true;
 
     AuroraBookingNFT(nftContractAddress).safeTransferFrom(
       lots[lotId].seller,
